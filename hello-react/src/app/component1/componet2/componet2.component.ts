74aef57fc8a9b695f9bec1e893d5d7f86c7637cd
dbhattacha28 <debayan.bhattacharya@dxc.com>
2020-01-08 15:57:10 +0530

    added 2 components

---
+++ hello-react/src/app/component1/componet2/componet2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componet2',
+  templateUrl: './componet2.component.html',
+  styleUrls: ['./componet2.component.css']
+})
+export class Componet2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
